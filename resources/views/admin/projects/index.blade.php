75f1ae29d9189ced131bbba9a79dc10b816c04db
sashapoliti <sas.politi@gmail.com>
2024-05-30 17:43:43 +0200

    add admin/projects view index with table and print of db projects

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="container mt-4">
+        <h1 class="text-center">Projects</h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                  <th scope="col">#ID</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Slug</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <th scope="row">{{ $project->id }}</th>
+                        <td>{{ $project->title }}</td>
+                        <td>{{ $project->description }}</td>
+                        <td>{{ $project->slug }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </section>
+@endsection
